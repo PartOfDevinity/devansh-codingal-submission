e1162e03a522967c0aa8f84078204fc94d96ae66
PartOfDevinity <devanshsharma11102011@gmail.com>
2026-05-28 20:13:30 +0530

    Add files via upload

---
+++ l2a1.js
@@ -0,0 +1,40 @@
+function greet(name){
+    
+    console.log("hello",name)
+}
+
+greet("devansh")
+
+function sum(a, b){
+    console.log(a + b)
+}
+
+sum(10,20)
+function divide(a,b){
+    console.log(a/b)
+}
+divide(2,7)
+function multiply(a,b){
+    return a*b
+}
+ans = multiply(10,3)
+console.log(ans)
+
+function subtract(a,b){
+    return a-b
+}
+ans= subtract(10,2)
+console.log(ans)
+
+function addition(a,b){
+    return a+b
+}
+ans= additon(20,10)
+console.log(ans)
+
+function divide(a,b){
+    return a/b
+}
+
+ans= divide(15,7)
+console.log(ans)
