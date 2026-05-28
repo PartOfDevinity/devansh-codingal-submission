834983e6fa4aa3b9f21ce4afb449600e95e6b78f
PartOfDevinity <devanshsharma11102011@gmail.com>
2026-05-28 20:15:52 +0530

    Add files via upload

---
+++ l1a1.js
@@ -0,0 +1,14 @@
+name="Devansh"
+age=15
+course="HTML and CSS"
+
+console.log("My name is: ",name)
+console.log("My age is: ",age)
+console.log("My course is: ",course)
+a=10
+b=3
+console.log("Addition:",a+b)
+console.log("Subtraction:",a-b)
+console.log("Multiplication:",a*b)
+console.log("Division:",a/b)
+console.log("Module:",a%b)
