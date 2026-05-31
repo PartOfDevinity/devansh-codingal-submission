8087b11333d6a44a2af58e9e9b67f1197f4fdc31
PartOfDevinity <devanshsharma11102011@gmail.com>
2026-05-31 21:19:36 +0530

    Add files via upload

---
+++ l4a1.js
@@ -0,0 +1,18 @@
+devanshobj ={
+    name: "devansh",
+    age: 15,
+    course: "java script",
+    hobby: "singing ",
+}
+
+console.log(devanshobj)
+console.log("full name of devansh :", devanshobj.name)
+console.log("age of devansh:",devanshobj.age)
+console.log("course taken by devansh :",devanshobj.course)
+console.log("hobby of devansh:",devanshobj.hobby)
+ 
+devansh.course ="html and CSS"
+console.log(devanshobj)
+ 
+devansh.school ="st xaviers high school"
+console.log(devanshobj)
