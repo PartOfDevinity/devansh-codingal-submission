18437adf106cf670f5f59bb796a3cd14ed0e51b4
PartofDivinity <devanshsharma11102011@gmail.com>
2026-06-30 19:20:56 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+print("Devansh here")
+name = "Devansh"
+age = 15 
+course = "python"
+ambition = "coding expert"
+favorite  = "kadai panner"
+
+print("my name is", name)
+print(f"my age is {age}")
+print(f"my course is {course}")
+print(f"my ambition is {ambition}")
+print(f"my favorite food {favorite }")
